05e0e2a7ab489684f6a13b54937cc809bb92efb8
hch <huachunhu@9188.com>
2019-04-29 15:43:51 +0800

    视觉优化

--- app/src/main/java/com/db/loan/loancenter/ui/activity/MainWebViewActivity.java
+++ app/src/main/java/com/db/loan/loancenter/ui/activity/MainWebViewActivity.java
@@ -1,5 +1,6 @@
 package com.db.loan.loancenter.ui.activity;
 
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.view.View;
@@ -23,5 +24,24 @@ public class MainWebViewActivity extends BaseWebActivity {
     protected void initConfig() {
         super.initConfig();
         setFullScreenFlag(false);
+        setStatusBarHintDarkGEM(true);
+    }
+
+    /**
+     * 设置状态栏的文字颜色
+     *
+     * @param darkcolor true : 设置为白色文字， false ： 设置为灰色文字
+     */
+    public void setStatusBarHintDarkGEM(boolean darkcolor) {
+        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.M) {
+            View decorView = this.getWindow().getDecorView();
+            int flag = View.SYSTEM_UI_FLAG_LAYOUT_STABLE;
+            if (darkcolor) {
+                flag |= View.SYSTEM_UI_FLAG_LIGHT_STATUS_BAR;
+            }
+
+            decorView.setSystemUiVisibility(flag);
+        }
+
     }
 }
