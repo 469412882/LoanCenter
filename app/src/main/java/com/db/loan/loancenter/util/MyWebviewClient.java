260d963fb803729dd4100569d1052b9f377af96e
hch <469412882@qq.com>
2019-07-14 23:22:13 +0800

    webview client拦截微信支付

--- app/src/main/java/com/db/loan/loancenter/util/MyWebviewClient.java
+++ app/src/main/java/com/db/loan/loancenter/util/MyWebviewClient.java
@@ -8,6 +8,9 @@ import android.net.Uri;
 import com.tencent.smtt.sdk.WebView;
 import com.yofish.kitmodule.base_component.webview.DefaultWebViewClient;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class MyWebviewClient extends DefaultWebViewClient {
     public MyWebviewClient(Context context) {
         super(context);
@@ -15,19 +18,27 @@ public class MyWebviewClient extends DefaultWebViewClient {
 
     @Override
     public void onPageStarted(WebView webView, String url, Bitmap bitmap) {
-        {
-            try {
-                if (url.startsWith("weixin://wap/pay?")) {
-                    Intent intent = new Intent();
-                    intent.setAction(Intent.ACTION_VIEW);
-                    intent.setData(Uri.parse(url));
-                    webView.getContext().startActivity(intent);
-                }
-            } catch (Exception e) {
-                e.printStackTrace();
-                //调起微信页面失败,自己看着办...
+        super.onPageStarted(webView, url, bitmap);
+    }
+
+    @Override
+    public boolean shouldOverrideUrlLoading(WebView webView, String url) {
+        try {
+            if (url.startsWith("weixin://wap/pay?")) {
+                Intent intent = new Intent();
+                intent.setAction(Intent.ACTION_VIEW);
+                intent.setData(Uri.parse(url));
+                webView.getContext().startActivity(intent);
+            } else {
+                Map<String, String> extraHeaders = new HashMap<>();
+                extraHeaders.put("Referer", "http://shensuan.xisuansuan.cn");
+                webView.loadUrl(url, extraHeaders);
             }
-            super.onPageStarted(webView, url, bitmap);
+            return true;
+        } catch (Exception e) {
+            e.printStackTrace();
+            //调起微信页面失败,自己看着办...
         }
+        return super.shouldOverrideUrlLoading(webView, url);
     }
 }
